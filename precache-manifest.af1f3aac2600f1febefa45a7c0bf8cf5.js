59b016d3a10353cbf62ea2d8dd943b86e19ad823
Muri68 <muriisyaku68@gmail.com>
2020-09-11 10:13:54 +0100

    Updates

---
+++ precache-manifest.af1f3aac2600f1febefa45a7c0bf8cf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae0e92cd3d0650156e15060630d2964",
+    "url": "/RoboFriends-With-React/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends-With-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c490a20aafc62664ff55",
+    "url": "/RoboFriends-With-React/static/css/main.3a56f6f3.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/RoboFriends-With-React/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends-With-React/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c490a20aafc62664ff55",
+    "url": "/RoboFriends-With-React/static/js/main.67e76d06.chunk.js"
+  },
+  {
+    "revision": "fe5204db2f37f6eec41c",
+    "url": "/RoboFriends-With-React/static/js/runtime-main.bda51c6c.js"
+  },
+  {
+    "revision": "170eca1f1da74462c2c36108b57c7be7",
+    "url": "/RoboFriends-With-React/static/media/Spirax-Regular.170eca1f.woff"
+  }
+]);
